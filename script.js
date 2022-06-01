ace87a16fb416de7eae1aaeb49a44fd95b01b2b4
Gu-Jack <1097549@apps.nsd.org>
2022-06-01 11:52:51 -0700

    Go to Git

---
+++ script.js
@@ -0,0 +1,10 @@
+const icons = document.querySelectorAll("i");
+var find = Math.random * icons.length;
+
+for(var i = 0; i < icons.length; i++) {
+    icons[i].addEventListener("click", {
+        if(i == find) {
+                
+        }
+    })
+}
